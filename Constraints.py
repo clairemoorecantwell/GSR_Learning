5de2992d1bcfbfdd9a90485b039c056df86ca4e3
Iuliia Zaborna <juliazabornaya@gmail.com>
2020-10-08 10:42:39 -0700

    added a function to read the input

--- Constraints.py
+++ Constraints.py
@@ -84,10 +84,10 @@ print(syllable_structure("ta:gi"))
 print(syllable_structure("tagi"))
 print(syllable_structure("tagi:"))
 '''
-print(prosody_violations("tagia", "tagi", "a") )    # violation  returns 1
-print(prosody_violations("tagiia", "tagi", "ia"))   #no violation  0
-print(prosody_violations("o:sia", "osi", "a"))      #no violation > 0
-print(prosody_violations("o:siina", "to", "ina"))  # violation 1 
+#print(prosody_violations("tagia", "tagi", "a") )    # violation  returns 1
+#print(prosody_violations("tagiia", "tagi", "ia"))   #no violation  0
+#print(prosody_violations("o:sia", "osi", "a"))      #no violation > 0
+#print(prosody_violations("o:siina", "to", "ina"))  # violation 1 
 
 
 def uniformity (candidate, suffix, root):
