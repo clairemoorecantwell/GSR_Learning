c23134b1f5d0dea4ec46657acf9035c3f58047db
clairemoorecantwell <e.nerilika@gmail.com>
2021-01-25 15:41:54 -0800

    Claire's big overhaul

--- Constraints.py
+++ Constraints.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Oct  3 12:32:22 2020
 
-@author: juliazaborna
-"""
 #HL roots should take single-syllable suffixes, -a, -ga, -i, etc.
 #LL and H roots should take two-syllable suffixes, -ia/-ina/-Cia, -aga, etc
 
@@ -12,116 +8,101 @@ Created on Sat Oct  3 12:32:22 2020
 #function takes three strings : candidate, root, suffix 
 
 import re
-import GenerateCandidate_function
-import GSRlearner as gsrln
 
-vowels = ['a', 'o', 'i', 'u', 'e']
 
 
 def syllable_structure (root_form):
-    
-    
-    syll_structure =[] #initialize list to store syll structure
+	vowels = ['a', 'o', 'i', 'u', 'e']    
+	syll_structure =[] #initialize list to store syll structure
+	
+	for i in range (0,len(root_form)):
+	   
+		if root_form[i] in vowels: #found a vowel
+			if i==(len(root_form)-1): #last vowel -> L
+				syll_structure.append("L")
+			else:                     # not the last vowel
+				if root_form [i+1] == ":": # if its a long vowel -> H
+					syll_structure.append("H")
+				else:                     # short vowel ->L
+					syll_structure.append("L")
+
+	syll_str_string = "".join(syll_structure) #join into a string 
+	
+	return syll_str_string
+
+
+def prosody(candidate, suffix=None):
+	#returns 1 if a candidate violates prosody, 0 otherwise
+	
+	if suffix is None:
+		return 0
+	
+	monosyllabic_suffix = ["a","ga","i"]
+	disyllabic_suffix = ["ia","ina","aga","aqi"]
+		
+	#to count for : togi but to:giina , root = candidate - suffix
+	   
+	suff_len = len(suffix)
+	root_form = candidate [:-suff_len] #modified root form = candidate- suffix
+			
+	#print(root_form)
+	syll_structure = syllable_structure(root_form)
    
-    for i in range (0,len(root_form)):
-       
-       if root_form[i] in vowels: #found a vowel
-           if i==(len(root_form)-1): #last vowel -> L
-               syll_structure.append("L")
-           else:                     # not the last vowel
-               if root_form [i+1] == ":": # if its a long vowel -> H
-                   syll_structure.append("H")
-               else:                     # short vowel ->L
-                   syll_structure.append("L")
-                   
-       
-           
-           
-    syll_str_string = "".join(syll_structure) #join into a string 
-    
-    return syll_str_string
+	
+	#if HL takes other than monosyll suffix > assign a violation   
+	if syll_structure == "HL" and suffix not in monosyllabic_suffix:
+		violation = 1
+	
+	#if LL takes other than disyllabic suffix > assing a violation
+	elif syll_structure == "LL"  and suffix not in disyllabic_suffix :
+		#if LL and -a
+		violation = 1
+			
+	elif syll_structure =="H"  and suffix not in disyllabic_suffix:
+		violation = 1
+	else:
+		violation = 0
+	
+	return violation 
 
 
-def prosody(candidate, root, suffix):
-    #returns 1 if a candidate violates prosody, 0 otherwise
-    
-    monosyllabic_suffix = ["a","ga","i"]
-    disyllabic_suffix = ["ia","ina","aga"]
-        
-    #to count for : togi but to:giina , root = candidate - suffix
-       
-    suff_len = len(suffix)
-    root_form = candidate [:-suff_len] #modified root form = candidate- suffix
-            
-    #print(root_form)
-    syll_structure = syllable_structure(root_form)
-   
-    
-    #if HL takes other than monosyll suffix > assign a violation   
-    if syll_structure == "HL" and suffix not in monosyllabic_suffix:
-        violation = 1
-    
-    #if LL takes other than disyllabic suffix > assing a violation
-    elif syll_structure == "LL"  and suffix not in disyllabic_suffix :
-        #if LL and -a
-        violation = 1
-            
-    elif syll_structure =="H"  and suffix not in disyllabic_suffix:
-        violation = 1
-    else:
-        violation = 0
-    
-    return violation 
-
-
-
-#print(prosody_violations("ta:giina", "tagi", "ina")) #no violation 
-    
-#test syllab_structure
-
-#print(syllable_structure("ta:gis"))
-'''
-print(syllable_structure("ta:gi"))
-print(syllable_structure("tagi"))
-print(syllable_structure("tagi:"))
-'''
-#print(prosody("tagia", "tagi", "a") )    # violation  returns 1
-
-#print(prosody("tolopo:ina", "tolopo", "ina"))
-#print(prosody_violations("tagiia", "tagi", "ia"))   #no violation  0
-#print(prosody_violations("o:sia", "osi", "a"))      #no violation > 0
-#print(prosody_violations("o:siina", "to", "ina"))  # violation 1 
 
 
 #uniformity looks for (rs) in the candidate string 
 #return 1 if there is a violation, 0 if there is no violation
 def uniformity (candidate):
-    
-    violation = 0
-    
-    rs = re.search('(rs)',candidate)
-    print(rs)
-    if rs:
-        violation =1
-        #if re returns a match violation =1
-    return violation
-
-#testing uniformity function#
-#CASE 1b:
-rt2 = [['tagis', 0.4]]
-rt_lex = gsrln.lexeme('tagi', rt2, 'root')
-
-sf2 = [['ina', 0.3], ['a', 0.3], ['sia', 0.3], ['tia', 0.2], ['fia', 0.4], ['mia', 0.3]]
-sf_lex = gsrln.lexeme('PV', sf2, 'suffix')
+	
+	violation = 0
+	
+	rs = re.search('(rs)',candidate.c)
+	
+	if rs:
+		violation =1
+		#if re returns a match violation =1
+	return violation
 
 
-cands =GenerateCandidate_function.generate_candidate(rt_lex, sf_lex)
+def OCP(candidate):
+	if re.search('(a{2}|e{2}|i{2}|o{2}|u{2})',candidate.c):
+		return(1)
+	else:
+		return(0)
+	
+def DEP(cand):
+	if cand.activityLevel > 1 or cand.activityLevel == 0: #if the candidate's activity level is greater than 1 or equals to 0, returns 0
+		return 0
+	else: # otherwise subtract the activity level from 1 and return
+		return 1-cand.activityLevel
+	
+def NoCoda(candidate):
+	if re.search('.*([^aeiou\)]|[^aeiou]\))$',candidate.c):
+		return(1)
+	else:
+		return(0)
+
+def MAX(cand):
+	return cand.activityLevel #each candidate's activity level equals their reward points 
 
 
-print (len(cands))
-
-for i in range(len(cands)):
-    print(cands[i].c)
-    print(uniformity(cands[i].c))
-
-
+con = [prosody,uniformity,OCP,DEP,NoCoda,MAX]
+conNames = ['prosody','uniformity','OCP','DEP','NoCoda','MAX']
--- GSRlearner.py
+++ GSRlearner.py
@@ -2,14 +2,16 @@
 from pandas import read_excel
 import numpy as np 
 import math
+import Constraints
+import re
 
 #####################################################
 
 
 class lexeme:
-    def __init__(self, tag, allomorphs, kind):
-        self.tag = tag # label for the lexeme, so the humans can easily see what it is.  ex. 'tagi' '-ina', even things like 'PV' or '3rdsing'
-        self.allomorphs = allomorphs # list of allomorphs, in the following format:
+	def __init__(self, tag, allomorphs, kind):
+		self.tag = tag # label for the lexeme, so the humans can easily see what it is.  ex. 'tagi' '-ina', even things like 'PV' or '3rdsing'
+		self.allomorphs = allomorphs # list of allomorphs, in the following format:
 
 # allomorph lists for roots only have one entry
 # w/ C
@@ -27,51 +29,113 @@ class lexeme:
 #  [ 'a', 0.0]
 # ]
 
-        self.kind = kind # string specifying what kind of morpheme it is.  'root' 'suffix' 'prefix' etc.
-        self.freq = 0   #initialize at zero, increase during learning
-        
-
+		self.kind = kind # string specifying what kind of morpheme it is.  'root' 'suffix' 'prefix' etc.
+		self.freq = 0   #initialize at zero, increase during learning
+		
 
 class Lexicon:
-    
-    def __init__(self):
-        self.lexemeList = []  #initialise empty list for storing lexemes in
-        self.freqList = []    #empty list for storing each lexeme's frequency - used for sampling
-        
-        
-    def add_lexeme(self,l):
-        # Add a lexeme to the lexemeList
-        self.lexemeList(l)
-        self.freqList.append(l.freq)
-        
-    
-    def update_freqList(self):
-        # update freqList with all lexemes' frequencies, in case they have changed
-        self.freqList = []
-        for l in self.lexemeList:
-            self.freqList.append(l.freq)
-    
-    def sample(self, n):
-        # sample 
-        #break
-        return 0
-            
-    
-            
-            
-            
-    
-    
-    
+	
+	def __init__(self,filename=None,sheetNo=1):
+		self.lexemeList = {}  # dict will have the structure {tag: lexeme}
+		self.freqList = []    #empty list for storing each lexeme's frequency - used for sampling
+		if filename is not None:
+			self.read_input(filename,sheetNo)
+		
+	def add_lexeme(self,l):
+		# Add a lexeme to the lexemeList
+		self.lexemeList[l.tag]=l
+		self.freqList.append(l.freq)
+		
+	
+	def update_freqList(self):
+		# update freqList with all lexemes' frequencies, in case they have changed
+		self.freqList = []
+		for l in self.lexemeList:
+			self.freqList.append(l.freq)
+	
+	def sample(self, n):
+		# sample 
+		#break
+		return 0
+			
+	def read_input(self,file_name, sheet_number):  # sheet_number starts from 1
+		# TODO add functionality to check if its an excel file or .txt; and then also be able to read .txt
+		# TODO further, add try... except to check for proper loading of pandas, and if not, throw up a useful error
+		#reading excel into dataframe
+		input_dt_frame = read_excel(file_name, sheet_number-1 ) #this is for the sheet#1; 
+			#dataframe into np array
+		input_np = input_dt_frame.to_numpy()
+		
+		cpyarr = input_np.tolist()
+		roots = []  # |
+		suffix = []
+		suffix_helper = []# |
+		them_c_helper = []
+		them_c = []
+		allomorphs = []
+		
+		# thematic c =(['t','n','f','g','m','l','s','q']) Note of list of all the thematic C's
+		for x in range (len(cpyarr)):  # looping through Julia's numpy array and separating roots, suffix, and them_c columns
+			roots.append(cpyarr[x][6])
+			suffix_helper.append(cpyarr[x][3])
+			them_c_helper.append(cpyarr[x][2])
+	
+		suffix = list(set(suffix_helper))
+		them_c = list(set(them_c_helper))
+		them_c = list(filter(lambda x: str(x)!= 'nan',them_c))  # to not generate something like "tagiNA"
 
-    
+		for x in range(len(roots)):  # looping through roots column
+			allomorphs = []
+			for y in them_c:  # concat each root with all possible them_c
+				allomorphs.append([roots[x]+y]+[0])
+			self.add_lexeme(lexeme(roots[x], allomorphs, 'root'))
 
-        
+		tag = 'ina'
+		allomorphs = [['ina',0],['a',0]]
+		for y in them_c:
+			allomorphs.append([y+'ia']+[0])
+		self.add_lexeme(lexeme(tag,allomorphs,'suffix'))
+		
+		tag = 'aga'
+		allomorphs = [['ga',0]]
+		for y in them_c:
+			allomorphs.append([y+'aga']+[0])
+		self.add_lexeme(lexeme(tag,allomorphs,'suffix'))
+			
+		tag = 'aqi'
+		allomorphs = []
+		for y in them_c:
+			allomorphs.append([y+'aqi']+[0])
+		self.add_lexeme(lexeme(tag,allomorphs,'suffix'))
 
-        
+		tag = 'i'
+		allomorphs = []
+		for y in them_c:
+			allomorphs.append([y+'i']+[0])
+		self.add_lexeme(lexeme(tag,allomorphs,'suffix'))
+		
+		
+# hard-coding suffixes for now
+
+#		for x in range(len(suffix)):  # looping through suffixes column
+#			suff_sets = [['ia','a','ina'],['aga','ga']]
+#			allomorphs = []
+#			if x in ['i','ia','aga','aqi']:  # could adjust this later to try C's on every form - would need to add *CC
+#				for y in them_c:  # concat each suffix with all possible them_c
+#					allomorphs.append([y+suffix[x]]+[0])
+#			for suff in suff_sets:
+#				if x in suff:
+#					for y in suff:
+#						if y != x:
+#							allomorphs.append(y)
+#			
+#			self.add_lexeme(lexeme(suffix[x], allomorphs, 'suffix'))
+   
+   
+   
   
 class Tableau: 
-	def __init__(self,inpt,prob=1,pfc=None):
+	def __init__(self,inpt,prob=1):
 		self.inpt = inpt # text specifying the input.  
 
 		self.candidates = [] # To be filled during tableau creation
@@ -82,6 +146,58 @@ class Tableau:
 								# gets filled (and updated) during learning
 		self.obsProbsList = [] 	# list of each candidate's observed probability; used in sampling
 
+
+	def generate_candidates(self,root, suffix, observed):
+		cand_list = []
+		
+		if root.kind != 'root':
+			return("Error: The root is not valid!")
+		if suffix.kind != 'suffix':
+			return("Error: The suffix is not valid!")
+		
+		nonClist = ['ina','a','ga','i']
+		
+		cand_list = []
+		NeedNoCcandidateStill = True
+		ss = ''
+		for s in suffix.allomorphs:
+			if s[0] in nonClist: # allomorphs without a C
+				# no thematic C
+				obs = 1 if root.allomorphs[0][0][:-1]+s[0]==observed else 0
+				cand_list.append(candidate(root.allomorphs[0][0][:-1]+s[0],[],obs,0.0,surfaceForm = root.allomorphs[0][0][:-1]+s[0]))
+				
+				# the (r) candidates
+				for r in root.allomorphs:
+					obs = 1 if r[0]+s[0]==observed else 0
+					cand_list.append(candidate(r[0]+'(r)'+s[0],[],obs,r[1],surfaceForm =r[0]+s[0]))
+					
+			elif NeedNoCcandidateStill:
+				# neither thematic C surfaces
+				obs = 1 if root.allomorphs[0][0][:-1]+s[0][1:]==observed else 0
+				cand_list.append(candidate(root.allomorphs[0][0][:-1]+s[0][1:],[],obs,0.0,surfaceForm =root.allomorphs[0][0][:-1]+s[0][1:]))
+				ss=s[0][1:]
+				NeedNoCcandidateStill = False
+			
+			else:
+				# the (s) candidate
+				obs = 1 if root.allomorphs[0][0][:-1]+s[0]==observed else 0
+				cand_list.append(candidate(root.allomorphs[0][0][:-1]+'(s)'+s[0],[],obs,s[1],surfaceForm =root.allomorphs[0][0][:-1]+s[0]))
+				
+				# (rs) candidates
+				for r in root.allomorphs:
+					rtC = r[0][-1]
+					sufC = s[0][0]
+					if rtC == sufC:  # if applicable, the (rs) candidate
+						obs = 1 if r[0]+s[0][1:]==observed else 0
+						cand_list.append(candidate(r[0]+'(rs)'+s[0][1:],[],obs,r[1]+s[1],surfaceForm =r[0]+s[0][1:]))
+						
+		for r in root.allomorphs:
+			# the (r) candidate
+			obs = 1 if r[0]+ss==observed else 0
+			cand_list.append(candidate(r[0]+'(r)'+ss,[],obs,r[1],surfaceForm =r[0]+ss))
+						
+				
+		return cand_list
 
 	def addCandidate(self,cand):
 		self.candidates.append(cand)
@@ -150,7 +266,19 @@ class Tableau:
 		winner = winCandidate.surfaceForm
 		return winner, winCandidate
 
-	def getObsWinner(self):
+
+	def getObsWinner_RIP(self,w):
+
+		# create sub-tableau
+		tab_rip = Tableau(self.inpt)
+		for c in self.candidates:
+			if c.observedProb > 0.0:
+				tab_rip.addCandidate(c)
+		tab_rip.predictProbs(w)
+		winner, winCandidate = tab_rip.getPredWinner()
+		return winner, winCandidate
+
+	def getObsWinner_sample(self):
 
 		# Get candidate list
 		# Get probability list
@@ -161,17 +289,18 @@ class Tableau:
 	def compareObsPred(self,w):
 		self.predictProbs(w)
 		pred, predCandidate=self.getPredWinner() # Sample from predicted distribution
-		obs, obsCandidate=self.getObsWinner() # Sample from observed distribution
+		obs, obsCandidate=self.getObsWinner_RIP(w) # Via Robust Interpretive parsing
 
 		error = (0 if obs==pred else 1)
 
 		return error, obsCandidate, predCandidate
-        
-        
+		
+		
   
 class candidate: 
-	def __init__(self,c,violations,observedProb,surfaceForm=None):
+	def __init__(self,c,violations,observedProb,activityLevel,surfaceForm=None):
 		self.c = c # the actual candidate 
+		self.surfaceForm = surfaceForm
 		self.violations = violations # list of violations, in same order as constraints
 		self.observedProb = observedProb # The observed probability of the candidate
 		try: # make sure observedProb is a float, or can be converted to float
@@ -181,99 +310,156 @@ class candidate:
 		self.harmony = 0 # Not specified at initialization.	 This will be filled out in learning
 		self.predictedProb = 0 # Again, to be filled out during learning.
 #		self.checkViolationsSign() # On initialization, make sure all violations are negative
-		self.activityLevel = 0.0 # float of activity level of thematic C of the candidate for calculating violations
+		self.activityLevel = activityLevel # float of activity level of thematic C of the candidate for calculating violations
 	
-	def applyConstraints(self, constraints):
-		# for i in constraints:
-		#  apply i(self.c)  <-- research, what is the best way to store list of functions.  lambda functions?  [min(),max()...]  [i(cand) for i in list]
-		#  append result to self.violations
-		#  self.violations.append(MAX(self.c))
-		#  ...
+	def applyConstraints(self, constraints=Constraints.con):
+		self.violations=[i(self) for i in constraints]
+		# TO DO: append, or overwrite?
 
 
 
 class Grammar:
-    def __init__(self,learningData):
-        self.learningData = learningData  # An array or something containing the learning data
-        self.constraints = [] # Read these off the top of the input data file.
-        self.w = [] # Weights, starts empty
-        self.initializeWeights() # Initialize weights when Tableaux object is created (to zero, or some other vector depending on how you call this function.)
-        self.t = 0	# For learning, t starts at 0
-        self.lexicon = Lexicon()  # Starting with an empty lexicon; can change later if we want
-        self.learningRate = 0.1
-        self.activityUpdateRate = 0.05
+	def __init__(self,file_name,sheet_number=1):
+		self.learningData = self.readLearningData(file_name,sheet_number)  # An array or something containing the learning data
+		self.constraints = Constraints.conNames # Read these off the top of the input data file.
+		self.w = [] # Weights, starts empty
+		self.initializeWeights() # Initialize weights when Tableaux object is created (to zero, or some other vector depending on how you call this function.)
+		self.t = 0	# For learning, t starts at 0
+		self.learningRate = 0.1
+		self.activityUpdateRate = 0.05
+		# self.lexicon also exists - created with readLearningData
 
 
-    def initializeWeights(self,w=None):
-        '''Function to initialize the weights - can take an argument, which is hopefully the same length as the number of constraints in the Tableaux object.  If it doesn't get that argument, it will initialize them to zero. '''
+	def readLearningData(self,file_name,sheet_number=1):
+		self.lexicon=Lexicon(file_name,sheet_number)
+		input_dt_frame = read_excel(file_name, sheet_number-1 ) #this is for the sheet#1;
+		input_np = input_dt_frame.to_numpy()
+		
+		return input_np
+	
+	
+	def createLearningPlaylist(self,n):
+		# n is total number of learning simulations
+		#First, create list of inputs, with frequencies
+		observeds = []
+		freqs = []
+		base_tags = []
+		suff_tags = []
+		
+		for i in self.learningData:
+			observeds.append(i[1])
+			freqs.append(i[5])
+			if i[5]=='nan':
+				print(i)
+			base_tags.append(i[6])
+			suff_tags.append(i[4])
+
+		
+		playlist_nums = list(np.random.choice(range(0,len(observeds)),n,p=[(i+1)/(sum(freqs)+len(freqs)) for i in freqs]))
+		playlist = []
+		for i in playlist_nums:
+			playlist.append([observeds[i],self.lexicon.lexemeList[base_tags[i]],self.lexicon.lexemeList[suff_tags[i]]])
+		
+		return playlist
+
+
+
+	def initializeWeights(self,w=None):
+		'''Function to initialize the weights - can take an argument, which is hopefully the same length as the number of constraints in the Tableaux object.  If it doesn't get that argument, it will initialize them to zero. '''
 		# TODO (low priority atm) Add functionality to initialize weights to random values
-        if w is None:
-            self.w = [0]*len(self.constraints)
-        else:
-            if len(w)==len(self.constraints):
-                self.w=w
-            else:
-                print("WARNING: you tried to initialize weights with a list that's not the same length as your list of constraints.	 This probably didn't work so well.")
+		if w is None:
+			self.w = [0]*len(self.constraints)
+		else:
+			if len(w)==len(self.constraints):
+				self.w=w
+			else:
+				print("WARNING: you tried to initialize weights with a list that's not the same length as your list of constraints.	 This probably didn't work so well.")
 				# This will print a warning, but it won't actually stop you from initializing constraints badly?  Maybe this is a problem that should be fixed.
    
-    def calculate_sample_weights(self,frequencies):
-    
-        #helper function; calculates weights for the weighted sample
-        #takes a list of word frequencies as input 
-        #returns a set of weights 
-        
-        total = sum(frequencies)
-        weights = [ round(freq / total, 10) for freq in frequencies] 
-        return weights
-            
-        
-    
-    
-    def sample(self, n,learning_data_object):
-    #returns a weighted sample 
-    # takes word list and freq list from grammar.learning_data object
-        
-        rows,columns = learning_data_object.shape 
-        frequencies = learning_data_object[:rows,4]
-    #calculate weights 
-        weights = calculate_sample_weights (frequencies)  
-        lexicon = list(learning_data_object[:rows,0])
-        weighted_sample = np.random.choice(lexicon,n,p=weights)
-        return weighted_sample
-        
-        #sample
-        #break
+	def calculate_sample_weights(self,frequencies):
+	
+		#helper function; calculates weights for the weighted sample
+		#takes a list of word frequencies as input 
+		#returns a set of weights 
+		
+		total = sum(frequencies)
+		weights = [ round(freq / total, 10) for freq in frequencies] 
+		return weights
+			
 
-#remove after testing 
 
-    def check_sample(self,sample,input_np):
-        rows,colums = input_np.shape
-        lexicon = list(input_np[:rows,0])
-        list_sample = list(sample) 
-        for word in lexicon:
-            print(str(word) + " occured " + str(list_sample.count(word)) + " times in the sample")
-        return 0
+	def update(self,datum): # datum is an entry from playlist
+		# Start with a learning datum
+		# Create a tableau
+		tab = Tableau(datum[0])
+		tab.candidates = tab.generate_candidates(datum[1],datum[2],datum[0])
+		for cand in tab.candidates:
+			try:
+				cand.applyConstraints()
+			except:
+				print(cand)
+				print(datum)
+				print(datum[1].allomorphs)
+				print(datum[2].allomorphs)
+			print(cand.c,cand.surfaceForm,cand.observedProb,cand.violations)
+		# Generate a prediction
+		e, obs, pred = tab.compareObsPred(self.w)
+		print(e,obs.c,pred.c,obs.violations,pred.violations)
+		if e:
+			#update general weight: perceptron update
+			self.w = [wt+(p-o)*self.learningRate for wt,p,o in zip(self.w,pred.violations,obs.violations) ]
+			
+			#update activity levels of correct thematic C, if any
+			# First, boost activity levels on thematic C's if an (r), (s), or (rs) candidate was observed (regardless of predicted candidate)
+			if re.search('\(r',obs.c): #if (r) or (rs) is the observed form)
+				C = re.search("(.)(\(r)",obs.c).group(1) # grab out the thematic C
+				for a in datum[1].allomorphs: # Find the thematic C in the allomorphs of the root that we are learning with
+					if a[0][-1]==C: 
+						a[1]+=self.activityUpdateRate
+						
+			if re.search('s\)',obs.c): #if (s) or (rs) is the observed form
+				C = re.search("(s\))(.)",obs.c).group(2) # grab out the thematic C
+				for a in datum[2].allomorphs: # Find the thematic C in the allomorphs of the root that we are learning with
+					if a[0][0]==C: 
+						a[1]+=self.activityUpdateRate
+						
+			# Next, decrease activity rate if pred form has a thematic C and obs. did not
+			if not re.search('\(',obs.c) and re.search('\(r',pred.c):
+				C = re.search("(.)(\(r)",pred.c).group(1) # grab out the thematic C
+				for a in datum[1].allomorphs: # Find the thematic C in the allomorphs of the root that we are learning with
+					if a[0][-1]==C: 
+						a[1]-=self.activityUpdateRate
+						a[1] = 0.0 if a[1]<0.0 else a[1] #bottom out activity levels at zero
+			
+			if not re.search('\(',obs.c) and re.search('s\)',pred.c):
+				C = re.search("(s\))(.)",pred.c).group(2) # grab out the thematic C
+				for a in datum[2].allomorphs: # Find the thematic C in the allomorphs of the root that we are learning with
+					if a[0][0]==C: 
+						a[1]-=self.activityUpdateRate
+						a[1] = 0.0 if a[1]<0.0 else a[1] #bottom out activity levels at zero
+		self.t+=1
+		return e
+		
+	
+	def epoch(self,playlist,niter,start=0):
+		errors = 0
+		for i in range(0,niter):
+			errors+=self.update(playlist[start+i])
+			print(start+i)
+			
+		error_rate = errors/niter
+		return error_rate
+		
+		
+		
+	def learn(self,nIterations,nEpochs):
+		
+		playlist = self.createLearningPlaylist(nIterations*nEpochs)
+		for i in range(0,nEpochs):
+			self.epoch(playlist,nIterations,start=nIterations*i)
 
-    def update(self):
-        return 0
-        
-        
-    def learn(self):
-        return 0
-    
-    def epoch(self):
-        return 0
-        
-    
-    
-def read_input(file_name, sheet_number):
-        
-    #reading excel into dataframe
-    input_dt_frame = read_excel(file_name, sheet_number ) #this is for the sheet#1; 
-        #dataframe into np array
-    input_np = input_dt_frame.to_numpy()
+	
 
-    return input_np
 
 
 #testing for read_input and sampling  
@@ -290,9 +476,9 @@ def read_input(file_name, sheet_number):
 
 
 
-    
+	
 
 
-        
-        
+		
+		
  
